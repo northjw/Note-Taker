7752d567ebc32512d8a565e013de22316af267f6
mini pony <northjw1@gmail.com>
2020-10-28 11:58:58 -0400

    moving server.jd

---
+++ Develop/sever.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Listener
+// ===========================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
