f746a6518ae16f1479347cc4a16183b7f086ea9e
mini pony <northjw1@gmail.com>
2020-11-11 17:07:49 -0500

    troubleshooting heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const htmlRoutes = require("./routes/htmlRoutes");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 80;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
